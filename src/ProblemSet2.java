940b2f4e925b519520fad655e341fde74a6d4b72
Ryan Wilson <rwilson@ucvts.org>
2018-09-21 14:13:56 -0400

    initial commit

---
+++ src/ProblemSet2.java
@@ -0,0 +1,30 @@
+public class ProblemSet2 {
+	public static void main (String[] args) {
+		ProblemSet2 ps2 = new ProblemSet2();
+		ps2.sayHello();
+		ps2.gradeMe();
+		ps2.groupUs();
+		ps2.info();
+		ps2.initials();
+	}
+	
+	public void sayHello() {
+		
+	}
+	
+	public void gradeMe() {
+		
+	}
+	
+	public void groupUs() {
+		
+	}
+	
+	public void info() {
+		
+	}
+	
+	public void initials() {
+		
+	}
+}
