d1b476e5a8ab1dc9038bb9af5fe3442f9a6d2c11
Ryan Wilson <rwilson@ucvts.org>
2018-09-21 20:44:45 -0400

    added instructions at top of file

--- src/ProblemSet2.java
+++ src/ProblemSet2.java
@@ -1,6 +1,23 @@
+/**
+ * INSTRUCTIONS.
+ * 
+ * Problem Set 2 will test your knowledge and understanding of arithmetic
+ * operators and String concatenation. You'll be asked use the Scanner to
+ * read user input from the keyboard, and use this input later to compute
+ * and print certain required values to the console. All of this is explained
+ * (and demonstrated, when appropriate) in the README file.
+ */
+
 public class ProblemSet2 {
+	
+	/**
+	 * 
+	 * You need not modify the main method.
+	 */
+	
 	public static void main (String[] args) {
 		ProblemSet2 ps2 = new ProblemSet2();
+		
 		ps2.sayHello();
 		ps2.gradeMe();
 		ps2.groupUs();
